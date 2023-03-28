4c46b63f1c9bf412ae28fad89a1a09113a127397
Ekaterina Khvan <ve.hvan@gmail.com>
2023-03-28 19:38:22 +0900

    Initial Commit

---
+++ Спринт 3. Задача/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Спринт 3. Задача
+//
+//  Created by Katya Khvan on 2023/03/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Спринт 3. ЗадачаUITests/_______3________UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  _______3________UITestsLaunchTests.swift
+//  Спринт 3. ЗадачаUITests
+//
+//  Created by Katya Khvan on 2023/03/28.
+//
+
+import XCTest
+
+final class _______3________UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
