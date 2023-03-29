71bfb555cb9515c1734fd4f2d7d9c773a3196ec4
Ekaterina Khvan <ve.hvan@gmail.com>
2023-03-29 20:31:03 +0900

    создала счетчик

--- Спринт 3. Задача/ViewController.swift
+++ Спринт 3. Задача/ViewController.swift
@@ -8,12 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    private var counter: Int = 0
 
+    @IBOutlet weak var text: UILabel!
+    @IBOutlet weak var buttonDOutlet: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        text.text = "\(counter)"
     }
 
-
+    @IBAction func buttonDidTap(_ sender: Any) {
+        counter += 1
+        text.text = "\(counter)"
+    }
+    
 }
 
